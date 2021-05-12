cc1181a82babfabc86ea7206868b0cc8485fa814
Ivan Faryga <ifaryga@gmail.com>
2021-05-13 01:16:41 +0300

    добавляет решенные задачи

--- js/exercises.js
+++ js/exercises.js
@@ -19,3 +19,105 @@
 // add(15, 27, 10);
 // add(10, 20, 30);
 // add(5, 10, 15);
+
+// ===============#9 - return =================
+
+// function add(a, b, c) {
+//   // Change code below this line
+
+//   return a + b + c;
+
+//   // Change code above this line
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// ===============#10 - шаблонные строки 2.0 =================
+
+// function makeMessage(name, price) {
+//   // Change code below this line
+//   name = Radar;
+//   price = 6150;
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+//   // Change code above this line
+//   return message;
+//   console.log(message);
+// }
+
+// ===============#11 - математические операторы 2.0 =================
+
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//   // Change code below this line
+//   const totalPrice = orderedQuantity * pricePerItem;
+
+//   // Change code above this line
+//   return totalPrice;
+// };
+
+// ===============#12 - заказ продукта 2.0 =================
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   // Change code below this line
+
+//   const message = `You ordered droids worth ${
+//     orderedQuantity * pricePerDroid + deliveryFee
+//   } credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+//   // Change code above this line
+//   return message;
+// }
+
+// console.log(makeOrderMessage(2, 100, 50));
+// console.log(makeOrderMessage(4, 300, 100));
+// console.log(makeOrderMessage(10, 70, 200));
+
+// ===============#14 Операторы сравнения '===' и '!==' =================
+
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+//   const isMatch = SAVED_PASSWORD === password;
+
+//   // Change code above this line
+//   return isMatch;
+// }
+
+// ===============#14 Операторы сравнения '===' и '!==' =================
+
+// function checkAge(age) {
+//   let message;
+
+//   if (age >= 18) {
+//     // Change this line
+//     message = "You are adult";
+//   } else {
+//     message = "You are minor";
+//   }
+
+//   return message;
+// }
+
+// console.log(checkAge(20));
+// console.log(checkAge(8));
+// console.log(checkAge(14));
+// console.log(checkAge(38));
+
+// ===============#16 склад товаров =================
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+//   if (ordered > available) {
+//     message = "Not enough goods in stock!";
+//   } else {
+//     message = "Order is processed, our manager will contact you.";
+//   }
+//   // Change code above this line
+//   return message;
+// }
+
+// ===============#16 склад товаров =================
