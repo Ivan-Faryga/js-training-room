ada1acf3a5744644cc3f487eddb2c3f591245ecd
Ivan Faryga <ifaryga@gmail.com>
2021-05-14 22:12:16 +0300

    добавляет задачи

--- js/exercises.js
+++ js/exercises.js
@@ -183,4 +183,100 @@
 //   return message;
 // }
 
-// ===============#21 else...if exercise 2 =================
+// ===============#22             =================
+
+function checkIfCanAccessContent(subType) {
+  const canAccessContent = subType === "pro" || subType === "vip"; // Change this line
+
+  return canAccessContent;
+}
+
+// ================= #24 evaluating discount  ==========================
+
+// function getDiscount(totalSpent) {
+//   const BASE_DISCOUNT = 0;
+//   const BRONZE_DISCOUNT = 0.02;
+//   const SILVER_DISCOUNT = 0.05;
+//   const GOLD_DISCOUNT = 0.1;
+//   let discount;
+//   // Change code below this line
+//   if (totalSpent >= 50000) {
+//     discount = GOLD_DISCOUNT;
+//   } else if (totalSpent >= 20000 && totalSpent < 50000) {
+//     discount = SILVER_DISCOUNT;
+//   } else if (totalSpent >= 5000 && totalSpent < 20000) {
+//     discount = BRONZE_DISCOUNT;
+//   } else {
+//     discount = BASE_DISCOUNT;
+//   }
+//   // Change code above this line
+//   return discount;
+// }
+
+// console.log(getDiscount(137000));
+// console.log(getDiscount(45300));
+// console.log(getDiscount(13800));
+// console.log(getDiscount(200));
+// console.log(getDiscount(36100));
+
+// ================= #25 evaluating discount  ==========================
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+
+//   message = ordered > available? "Not enough goods in stock!" : "The order is placed, the manager will contact you";
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+// console.log(message);
+
+// ================= #27 applying switch  ==========================
+
+// function getSubscriptionPrice(type) {
+//   let price;
+//   // Change code below this line
+
+//  switch (type) { // Change this line
+//     case 'starter': // Change this line
+//       price = 0; // Change this line
+//       break;
+
+//     case 'professional': // Change this line
+//       price = 20; // Change this line
+//       break;
+
+//     case 'organization': // Change this line
+//       price = 50; // Change this line
+//       break;
+//   }
+
+//   // Change code above this line
+//   return price;
+// }
+
+// ================= #28 applying switch with default ==========================
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   let message;
+//   // Change code below this line
+
+//   switch (password) {
+//     case null:
+//       message = "Canceled by user!";
+//       break;
+
+//     case ADMIN_PASSWORD:
+//       message = "Welcome!";
+//       break;
+
+//     default:
+//       message = "Access denied, wrong password!";
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
